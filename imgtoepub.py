cb9ac101050946754aa9bd441936c01bcb3f9137
Shelby Munsch <smunsch@gmail.com>
2014-11-29 11:21:13 -0800

    yep

--- imgtoepub.py
+++ imgtoepub.py
@@ -1,21 +1,17 @@
 import argparse
-
-def thedir():
-    print "meow"
-
-def noimg():
-    #run when they flag that the dir isn't images
-    print "moo"
-def doimg():
-    #run when they don't flag that the dir isn't images
-    print "mao!"
+import shutil
 
 
 parser = argparse.ArgumentParser(description='Program to convert folder of images to epub.')
-parser.add_argument('thedir', help='The directory of files to take and turn into an epub', type=int)
-parser.add_argument('--not', '-n', action='store_true', help='If included, the directory is not images')
-
+parser.add_argument('thedir', help='The directory of files to take and turn into an epub')
+parser.add_argument('--not', '-n', dest='uhno', action='store_true', help='If included, the directory is not images')
 
 args = parser.parse_args()
 
-print args.thedir
+if args.uhno==True:
+    #if it's not images
+    print "true"
+    shutil.copy(args.thedir, "../moo")
+else:
+    #if it is images
+    print "false"
--- readme.md
+++ readme.md
@@ -3,3 +3,11 @@
 ##This is going to be a program to take a folder of images and make it an epub.
 
 # Looking for contributors...
+
+# Todo:
+- specify dir and file name to create
+- grab files in dir and put in tmp folder
+- create HTML
+- make file metadata
+- zip dir contents
+- create .epub from zip
