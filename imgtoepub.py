4c1b7dd6275fb3a0073b1fa3c2adc67314efcccd
Shelby Munsch <smunsch@gmail.com>
2014-11-29 11:14:50 -0800

    Trying to learn argparse

--- imgtoepub.py
+++ imgtoepub.py
@@ -1,1 +1,21 @@
-# Program to convert folder of images to epub
+import argparse
+
+def thedir():
+    print "meow"
+
+def noimg():
+    #run when they flag that the dir isn't images
+    print "moo"
+def doimg():
+    #run when they don't flag that the dir isn't images
+    print "mao!"
+
+
+parser = argparse.ArgumentParser(description='Program to convert folder of images to epub.')
+parser.add_argument('thedir', help='The directory of files to take and turn into an epub', type=int)
+parser.add_argument('--not', '-n', action='store_true', help='If included, the directory is not images')
+
+
+args = parser.parse_args()
+
+print args.thedir
