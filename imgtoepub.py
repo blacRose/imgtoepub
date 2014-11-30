577b925bf74983f5409daa6df9353d75270177ef
Shelby Munsch <smunsch@gmail.com>
2014-11-30 08:56:03 -0800

    todo?

--- imgtoepub.py
+++ imgtoepub.py
@@ -1,7 +1,6 @@
 import argparse
 import shutil
 
-
 parser = argparse.ArgumentParser(description='Program to convert folder of images to epub.')
 parser.add_argument('thedir', help='The directory of files to take and turn into an epub')
 parser.add_argument('--output', '-o', help='Filename to create') #output name, including epub or not
@@ -12,7 +11,6 @@ if args.output.endswith(".epub"):
     outfile = args.output[:-5]
 else:
     outfile = args.output
-
 
 if args.uhno==True:
     #if it's not images
