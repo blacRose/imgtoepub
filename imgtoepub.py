f68497db3e90c7cc42879d243eb162ac38987412
Shelby Munsch <smunsch@gmail.com>
2014-11-30 10:17:26 -0800

    doing outputdir

--- imgtoepub.py
+++ imgtoepub.py
@@ -1,5 +1,7 @@
 import argparse
 import shutil
+import tempfile
+import os
 
 parser = argparse.ArgumentParser(description='Program to convert folder of images to epub.')
 parser.add_argument('thedir', help='The directory of files to take and turn into an epub')
@@ -16,7 +18,12 @@ if args.output != None:
 
 if args.uhno==True:
     #if it's not images
-    print(outfile)
+    print("moo")
 else:
     #if it is images
-    print("tree")
+    dirtoputin = tempfile.gettempdir()
+    dirtoputin += "/img2epub"
+    print("Temporary files are in "+dirtoputin)
+    os.mkdir(dirtoputin)
+    
+    
