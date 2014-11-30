bcdf6537602e8dbf044348bb298124faebd32921
Shelby Munsch <smunsch@gmail.com>
2014-11-30 11:13:50 -0800

    commit messages are for bitches.

--- imgtoepub.py
+++ imgtoepub.py
@@ -11,7 +11,7 @@ from time import gmtime, strftime
 # f.close()
 # fn = f.name + '.gz'
 # os.rename(f.name, fn)
-# fz = gzip.open(fn, 'wb')
+# fz = gzip.open(fn, 'wt')
 # writer = csv.writer(fz, delimiter='\t', lineterminator=lt)
 # for row in table:
 #     writer.writerow(row)
@@ -21,17 +21,19 @@ parser = argparse.ArgumentParser(description='Program to convert folder of image
 parser.add_argument('thedir', help='The directory of files to take and turn into an epub')
 parser.add_argument('--output', '-o', help='Filename to create') #output name, including epub or not
 parser.add_argument('--not', '-n', dest='uhno', action='store_true', help='If included, the directory is not images')
+
 # cp -r for python
 def copyanything(src, dst):
     try:
         shutil.copytree(src, dst)
-    except OSError as exc: # python >2.5
+        print(src)
+    except OSError as exc:
         if exc.errno == errno.ENOTDIR:
             shutil.copy(src, dst)
         else: raise
 
 args = parser.parse_args()
-
+thedir = os.path.abspath(args.thedir)
 if args.output != None:
     if args.output.endswith(".epub"):
         outfile = args.output[:-5]
@@ -45,8 +47,7 @@ else:
     #if it is images
     dirtoputin = tempfile.gettempdir()
     dirtoputin += "/img2epub" + strftime("%Y-%m-%d[%H:%M:%S]", gmtime())
-    print("Temporary files are in "+dirtoputin)
-    copyanything(args.thedir, dirtoputin)
+    copyanything(thedir, dirtoputin)
     
     shutil.rmtree(dirtoputin)
     
